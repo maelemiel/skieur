ea17d0bdb46a8a2e3baf4343b3dcd3c6cd9f95f6
rchevallier1234 <114905166+rchevallier1234@users.noreply.github.com>
2023-03-26 03:38:01 +0200

    generate obstacle

---
+++ Assets/delet_zone.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class delet_zone : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Destroy(collision.gameObject);
+    }
+}
--- Assets/spawn.cs
+++ Assets/spawn.cs
@@ -4,16 +4,42 @@ using UnityEngine;
 
 public class spawn : MonoBehaviour
 {
+    public GameObject spawn2;
+    public GameObject spike;
+    public GameObject cube;
+    private GameObject newObject;
+    private int rand;
+    private double time;
+    int test;
     // Start is called before the first frame update
     void Start()
     {
-        GameObject cube2 = GameObject.Find("cube");
-        GameObject spike2 = GameObject.Find("spike");
-        spike2.transform.position = GameObject.Find("spwan").transform.position;
+        time = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-                
+        time = time + 0.001;
+        if (time >= 1)
+        {
+            creat_obstacle();
+            time = 0;
+        }
+        print(time);
     }
+    void creat_obstacle()
+    {
+        rand = Random.Range(0, 3);
+        if (rand == 1)
+        {
+            newObject = Instantiate(spike);
+            newObject.transform.position = spawn2.transform.position;
+        }
+        if (rand == 2)
+        {
+            newObject = Instantiate(cube);
+            newObject.transform.position = spawn2.transform.position;
+        }
+    }
+}
